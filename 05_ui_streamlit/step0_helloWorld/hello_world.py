a9e0a3c10c995b4030c78d81824ddacb4e3dc426
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2024-01-05 00:31:19 -0800

    ui streamlit hello world step

---
+++ 05_ui_streamlit/step0_helloWorld/hello_world.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("Hello World From Shawaal")
+st.text('Fixed width text')
+st.markdown('_Markdown_ **Pakistan**')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects')
+st.write(['st', 'is <', 3])
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
---
+++ 05_ui_streamlit/step0_helloWorld/readme.md
@@ -0,0 +1,10 @@
+# Streamlit Hello World
+
+Install streamlit package
+
+    pip install streamlit    
+
+ Create hello.py and run it:   
+    
+    streamlit run hello_world.py
+
