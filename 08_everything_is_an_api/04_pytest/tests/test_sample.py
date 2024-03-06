6189b72499646251cc2574c250d63278e130924a
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2024-03-06 22:18:22 +0500

    pytest step completed

---
+++ 08_everything_is_an_api/04_pytest/tests/test_sample.py
@@ -0,0 +1,9 @@
+def func(x):
+    return x+1
+
+def test_answer():
+    assert func(3) == 5   # assert keyword means i want answer 5 == 5 but here 4 == 5
+
+
+
+# To run this file pytest filename.py on anaconda terminal
