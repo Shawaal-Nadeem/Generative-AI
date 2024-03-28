3ba70f5dc7cde4cae974d63950d43ff90e78751e
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2024-03-28 05:20:35 +0500

    annotated types add

--- 04_everything_is_an_api/08_fastapi_todo/CRUD_FastAPI/crud_fastapi/main.py
+++ 04_everything_is_an_api/08_fastapi_todo/CRUD_FastAPI/crud_fastapi/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 from sqlmodel import Field, SQLModel, Session, create_engine
+from typing import Annotated
 import os
 
 
@@ -23,12 +24,12 @@ def get_Session():
         yield session
 
 @app.get("/todos")
-def get_todos(session=Depends(get_Session),):
+def get_todos(session:Annotated[dict,Depends(get_Session)]):
     todos = session.query(Todo).all()
     return todos
 
 @app.post("/todos")
-def create_todo(todo: CreateTodo, session=Depends(get_Session)):
+def create_todo(todo: CreateTodo, session:Annotated[dict,Depends(get_Session)]):
     todo = Todo.model_validate(todo)
     session.add(todo)
     session.commit()
@@ -36,14 +37,14 @@ def create_todo(todo: CreateTodo, session=Depends(get_Session)):
     return todo
 
 @app.delete("/todos/{id}")
-def delete_todo(id: int, session=Depends(get_Session)):
+def delete_todo(id: int, session:Annotated[dict,Depends(get_Session)]):
     todo_delete = session.get(Todo,id)
     session.delete(todo_delete)
     session.commit()
     return {"message": "Todo deleted"}
 
 @app.put("/todos/{id}")
-def update_todo(id: int, todo: CreateTodo, session=Depends(get_Session)):
+def update_todo(id: int, todo: CreateTodo, session:Annotated[dict,Depends(get_Session)]):
     todo_update = session.get(Todo, id)
     todo_update.title = todo.title
     todo_update.description = todo.description
