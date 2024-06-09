7aad3343ebd799a44d1b103df099651faa52cb12
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2024-06-09 15:26:23 +0500

    Kafka integration with Docker and FastAPI

--- 05_all_in_one_microservices/cloud_dev_docker/Docker/class1_poetry/class1_poetry/main.py
+++ 05_all_in_one_microservices/cloud_dev_docker/Docker/class1_poetry/class1_poetry/main.py
@@ -3,7 +3,32 @@ from sqlmodel import Field, SQLModel, Session, create_engine, select
 from typing import Annotated
 from .settings import conn_str  
 
-print(conn_str)
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+import asyncio
+
+async def consume(topic:str, bootstrap_servers:str):
+    consumer = AIOKafkaConsumer(
+        topic,
+        bootstrap_servers=bootstrap_servers,
+        group_id="my-group")
+    # Get cluster layout and join group `my-group`
+    await consumer.start()
+    try:
+        # Consume messages
+        async for msg in consumer:
+            print("consumed: ", msg.topic, msg.partition, msg.offset,
+                  msg.key, msg.value, msg.timestamp)
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
+
+
+def lifespan(app:FastAPI):
+    print("Consumer Start .....")
+    asyncio.create_task(consume(topic="todos", bootstrap_servers="broker:19092"))
+    yield
+
+# print(conn_str)
 
 class Todo(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
@@ -15,7 +40,8 @@ class CreateTodo(SQLModel):
     description: str
 
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
 
 def get_Session():
     engine = create_engine(conn_str)
@@ -29,12 +55,24 @@ def get_todos(session:Annotated[dict,Depends(get_Session)]):
     return todos
 
 @app.post("/todos")
-def create_todo(todo: CreateTodo, session:Annotated[dict,Depends(get_Session)]):
-    todo = Todo.model_validate(todo)
-    session.add(todo)
-    session.commit()
-    session.refresh(todo)
-    return todo
+async def create_todo(todo: CreateTodo, session:Annotated[dict,Depends(get_Session)]):
+    producer = AIOKafkaProducer(bootstrap_servers='broker:19092')
+    # Get cluster layout and initial topic/partition leadership information
+    await producer.start()
+    try:
+        todo = Todo.model_validate(todo)
+        # Produce message
+        await producer.send_and_wait("todos", todo.model_dump_json().encode('utf-8'))
+    finally:
+        # Wait for all pending messages to be delivered or expire.
+        await producer.stop()
+
+    return todo.model_dump_json()       
+    # todo = Todo.model_validate(todo)
+    # session.add(todo)
+    # session.commit()
+    # session.refresh(todo)
+    # return todo
 
 @app.delete("/todos/{id}")
 def delete_todo(id: int, session:Annotated[dict,Depends(get_Session)]):
